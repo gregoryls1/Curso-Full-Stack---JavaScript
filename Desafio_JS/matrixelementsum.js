f555e24fa28e105278a0be4d0b8fe4762a49e9c9
gregoryls1 <contatogregory@gmail.com>
2018-09-19 15:44:59 -0300

    solução desafio Matrix Elements Sum

---
+++ Desafio_JS/matrixelementsum.js
@@ -0,0 +1,26 @@
+//After they became famous, the CodeBots all decided to move to a new building and live together.
+//The building is represented by a rectangular matrix of rooms. 
+//Each cell in the matrix contains an integer that represents the price of the room. 
+//Some rooms are free (their cost is 0), but that's probably because they are haunted,
+//so all the bots are afraid of them. That is why any room that is free or is located 
+//anywhere below a free room in the same column is not considered suitable for the bots to live in.
+
+//Help the bots calculate the total price of all the rooms that are suitable for them
+
+
+function matrixElementsSum(matrix) {
+    let price = 0
+    
+    let exclude = []
+    for(let i = 0; i < matrix.length; i++){
+        for(let j = 0; j < matrix[i].length; j++){
+            if(matrix[i][j] === 0){
+                exclude.push(j)
+            } 
+            else if(exclude.indexOf(j) === -1){
+                price += matrix[i][j]
+            }
+        }
+    }
+    return price
+}
